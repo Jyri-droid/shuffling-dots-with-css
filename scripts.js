e2d2a87d6078d6ec98258f143fc985d411d392cb
Jyri Konttinen <62062679+Jyri-droid@users.noreply.github.com>
2021-08-19 15:54:38 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,55 +1,55 @@
 let colors = ["lightsalmon", "lightseagreen", "lightskyblue", "lightgoldenrodyellow", "lightgrey", "lightpink"];
-let subContainers = [];
+let dotContainers = [];
 let dots = [];
 
 // Create dots
 for (i = 0; i < colors.length; i++) {
 	// Create a subcontainer for each dot
-	subContainers[i] = document.createElement("div");
-	subContainers[i].style.border = "4px solid transparent";
-	subContainers[i].style.paddingLeft = countPadding(colors, i);
-	document.getElementById("container").appendChild(subContainers[i]);
+	dotContainers[i] = document.createElement("div");
+	dotContainers[i].style.border = "4px solid transparent";
+	dotContainers[i].style.paddingLeft = countPadding(colors, i);
+	document.getElementById("container").appendChild(dotContainers[i]);
 	// Create dots inside their containers
 	dots[i] = document.createElement("div");
 	dots[i].classList.add("dot");
 	dots[i].style.backgroundColor = colors[i];
-	subContainers[i].appendChild(dots[i]);
+	dotContainers[i].appendChild(dots[i]);
 }
 
 function shuffleDots() {
 	// Shuffle order in random
-	for (j = 0; j < subContainers.length; j++) {
-		let randomSwitch = Math.floor(Math.random() * (subContainers.length - j)) + j;
-		[[subContainers[j]],subContainers[randomSwitch]] = [[subContainers[randomSwitch]],subContainers[j]];
+	for (j = 0; j < dotContainers.length; j++) {
+		let randomSwitch = Math.floor(Math.random() * (dotContainers.length - j)) + j;
+		[[dotContainers[j]],dotContainers[randomSwitch]] = [[dotContainers[randomSwitch]],dotContainers[j]];
 	}
 	// Reposition dots
-	for (k = 0; k < subContainers.length; k++) {
-		subContainers[k].style.paddingLeft = countPadding(colors, k);
+	for (k = 0; k < dotContainers.length; k++) {
+		dotContainers[k].style.paddingLeft = countPadding(colors, k);
 	}
 }
 
-// Count left padding for subcontainers
+// Count left padding for dotContainers
 function countPadding(array, positionInArray) {
 	let step = 100 / array.length;
 	return positionInArray * step + (step / 2) + "%";
 }
 
-// Show or hide dots' container divs
+// Show or hide dot container divs
 function layerVisibility() {
 	let button = document.getElementById("buttonLayer");
-	if (subContainers[0].style.borderColor !== "transparent") {
+	if (dotContainers[0].style.borderColor !== "transparent") {
 		button.innerHTML = "Show layers";
-		for (l of subContainers) {
+		for (l of dotContainers) {
 			l.style.borderColor = "transparent";
 			l.style.marginTop = "0";
 		}
 	} else {
 		let distance = 0;
 		button.innerHTML = "Hide layers";
-		for (l of subContainers) {
+		for (l of dotContainers) {
 			l.style.borderColor = l.firstChild.style.backgroundColor;
 			l.style.marginTop = distance + "px";
-			distance = distance + subContainers[0].offsetHeight;
+			distance = distance + dotContainers[0].offsetHeight;
 		}
 	}
 }
